4e8ecee53bfa9785d585e2a83ce1e652b159e89d
Michelle Arellano <michelle.arellanofig@gmail.com>
2022-04-27 18:26:05 -0500

    Strings and chars

--- 03_Strings_And_Variables/src/main/kotlin/Main.kt
+++ 03_Strings_And_Variables/src/main/kotlin/Main.kt
@@ -1,6 +1,11 @@
 fun main(args: Array<String>) {
+    //This are strings
     println("The answer is \"yes\" so you can proceed")
     println("I like to put a \\ in my code")
+
+    //This are chars
+    println('A')
+    println('c')
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
